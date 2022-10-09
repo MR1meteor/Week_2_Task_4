132b15782a4b9fc32d73722235ebeae67112965b
Alexey Safronov <alexey-safronov@inbox.ru>
2022-10-09 15:55:10 +0300

    Добавьте файлы проекта.

---
+++ Task4v2/Product.cs
@@ -0,0 +1,42 @@
+﻿namespace Task4v2
+{
+    public class Product
+    {
+        public static int ProductsQuantity = 0;
+        public int ID { get; }
+        public int Price { get; private set; }
+        public int Quantity { get; private set; }
+        public string Name { get; private set; }
+
+        public Product(int price, int quantity, string name)
+        {
+            ProductsQuantity++;
+            ID = ProductsQuantity;
+            Price = price;
+            Quantity = quantity;
+            Name = name;
+        }
+
+        public Product(Product product)
+        {
+            ProductsQuantity++;
+            ID = ProductsQuantity;
+            Price = product.Price;
+            Quantity = product.Quantity;
+            Name = product.Name;
+        }
+
+        public void SetData(string name) 
+        { 
+            Name = name;
+            Console.WriteLine($"Имя товара {ID} сменено на '{Name}'");
+        }
+
+        public void SetData(int price, int quantity) 
+        { 
+            Price = price;
+            Quantity = quantity;
+            Console.WriteLine($"Цена товара {ID} изменена и стала '{Price}', количество товара теперь '{Quantity}'");
+        }
+    }
+}
---
+++ Task4v2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task4v2
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Product chocolate = new Product(28, 10, "Шоколадка");
+            Product bun = new Product(chocolate);
+
+            chocolate.SetData(70, 30);
+            bun.SetData("Булочка");
+        }
+    }
+}
